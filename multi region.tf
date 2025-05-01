ca75dbdfd1f19c4ec69b57f839d28c04dce6128f
deepa2664 <deepa261@gmail.com>
2025-05-01 16:15:39 +0530

    Update multi region.tf

--- multi region.tf
+++ multi region.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  alias = "primary region"
+  alias = "primaryregion"
   region = "us-east-1"
 }
 
@@ -11,11 +11,11 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0123456789abcdef0"
   instance_type = "t2.micro"
-  provider = "aws.primary region"
+  provider = aws.primaryregion
 }
 
 resource "aws_instance" "example2" {
   ami = "ami-0123456789abcdef0"
   instance_type = "t2.micro"
-  provider = "aws.secondary"
+  provider = aws.secondary
 }

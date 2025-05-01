86f84249c78ac7009bb488f2bf9b1163d42fb207
deepa2664 <deepa261@gmail.com>
2025-05-01 21:35:17 +0530

    Create variable.tf

---
+++ s3/variable.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  default     = "dev"
+}
